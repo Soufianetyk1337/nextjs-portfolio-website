7fffe5b0858a576dcc1f470d18d83793fb678f70
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-05 03:08:00 +0100

    Adding Projects and Technologies Data

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,41 +1,259 @@
-export const projects = [
-  {
-    title: "Personal Portfolio",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, qui.",
-    image: "/url/image.png",
-    tags: ["Nextjs", "styled-components"],
-    type: "frontend",
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 0,
-  },
-  {
-    title: "Ecommerce From Scratch",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, qui.",
-    image: "/url/image.png",
-    tags: ["React", "Firebase", "Stripe", "Redux-Saga"],
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 1,
-  },
-];
+import {
+  DiReact,
+  DiNodejs,
+  DiHtml5,
+  DiJsBadge,
+  DiCss3,
+  DiDocker,
+  DiGit,
+  DiVisualstudio,
+  DiMongodb,
+} from 'react-icons/di';
+import {
+  SiMaterialui,
+  SiRedux,
+  SiExpress, SiSwagger,
+} from 'react-icons/si';
 
-export const TimeLineData = [
+export const githubProjects = [{
+  image: {
+    src: '/images/cryptosnews_app.jpg',
+    alt: 'Crypronews website image',
+    width: 270,
+    height: 180,
+    layout: 'responsive',
+  },
+  projectType: 'Front End',
+  title: 'Crypto News App',
+  description: 'Cryptocurrencies app that keeps you update with prices and news of every coin and top exchanges website so you can start trading',
+  tags: [
+    {
+      name: 'React',
+      link: 'https://beta.reactjs.org/',
+    }, {
+      name: 'Material-ui',
+      link: 'https://mui.com/',
+    },
+    {
+      name: 'ChartJS',
+      link: 'https://www.chartjs.org/',
+    },
+    {
+      name: 'Redux-toolkit',
+      link: 'https://redux-toolkit.js.org/',
+    },
+  ],
+  source: 'https://github.com/Soufianetyk1337/cryptocurrency-app-with-react-redux-toolkit',
+  link: 'https://cryptosnews.vercel.app/',
+  id: 0,
+},
+{
+  image: {
+    src: '/images/express-restapi-docs.jpg',
+    alt: 'express rest api iamge',
+    width: 270,
+    height: 180,
+    layout: 'responsive',
+  },
+  projectType: 'Back End',
+  title: 'Express REST API',
+  description: 'REST API for users that offers a lot of functionalities like login, reset password, sessions, and rate-limiting the API so that it cant get DDosed',
+  tags: [
+    {
+      name: 'Express',
+      link: 'https://expressjs.com/',
+    },
+    {
+      name: 'MongoDB',
+      link: 'https://www.mongodb.com/',
+    }, {
+      name: 'Redis',
+      link: 'https://redis.io/',
+    },
+    {
+      name: 'Docker',
+      link: 'https://www.docker.com/',
+    },
+    {
+      name: 'Nginx',
+      link: 'https://www.nginx.com/',
+    }, {
+      name: 'Swagger',
+      link: 'https://swagger.io/',
+    },
+  ],
+  source: 'https://github.com/Soufianetyk1337/react-graphql-firebase-ecommerce-store',
+  link: 'https://express-restapi.xyz/',
+  id: 1,
+},
+{
+  image: {
+    src: '/images/Ecommerce_App.jpg',
+    alt: 'Ecommerce website image',
+    width: 270,
+    height: 180,
+    layout: 'responsive',
+  },
+  projectType: 'Full Stack',
+  title: 'Ecommerce Store',
+  description: 'Ecommerce store built from scratch where you can shop items by category and add them to your cart and perform payments ',
+  tags: [
+    {
+      name: 'React',
+      link: 'https://beta.reactjs.org/',
+    },
+    {
+      name: 'Firebase',
+      link: 'https://firebase.google.com/',
+    }, {
+      name: 'Stripe',
+      link: 'https://stripe.com/',
+    },
+    {
+      name: 'Redux',
+      link: 'https://redux.js.org/',
+    },
+    {
+      name: 'Redux-saga',
+      link: 'https://redux-saga.js.org/',
+    }, {
+      name: 'Formik',
+      link: 'https://formik.org/',
+    },
+  ],
+  source: 'https://github.com/Soufianetyk1337/REST-API-with-NodeJS-Express-MongoDB',
+  link: '',
+  id: 2,
+},
+];
+const styles = {
+  background: '#fff',
+  color: '#3E0E12',
+  display: 'flex',
+  alignItems: 'center',
+  height: '100%',
+};
+
+export const TechnologiesAndTools = [
+
   {
-    year: 2019,
-    text:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, qui.",
+    Name: 'HTML5',
+    Badge: (<DiHtml5
+      size="3rem"
+      style={
+        styles
+      }
+    />),
   },
   {
-    year: 2020,
-    text:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, qui.",
+    Name: 'CSS3',
+    Badge: <DiCss3
+      size="3rem"
+      style={
+        styles
+      }
+    />,
   },
   {
-    year: 2021,
-    text:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, qui.",
+    Name: 'JavaScript',
+    Badge: <DiJsBadge
+      size="3rem"
+      style={
+        styles
+      }
+    />,
+  },
+
+  {
+    Name: 'React',
+    Badge: (<DiReact
+      size="3rem"
+      style={
+        styles
+      }
+    />
+    ),
+  },
+  {
+    Name: 'Redux',
+    Badge: <SiRedux
+      size="3rem"
+      style={
+        styles
+      }
+    />,
+  },
+  {
+    Name: 'Material-ui',
+    Badge: <SiMaterialui
+      size="3rem"
+      style={
+        styles
+      }
+    />,
+  },
+  {
+    Name: 'NodeJS',
+    Badge: (<DiNodejs
+      size="3rem"
+      style={
+        styles
+      }
+    />),
+  },
+  {
+    Name: 'Express',
+    Badge: (<SiExpress
+      size="3rem"
+      style={
+        styles
+      }
+    />),
+  },
+  {
+    Name: 'MongoDB',
+    Badge: (<DiMongodb
+      size="3rem"
+      style={
+        styles
+      }
+    />),
+  },
+  {
+    Name: 'Docker',
+    Badge: <DiDocker
+      size="3rem"
+      style={
+        styles
+      }
+    />,
+  },
+
+  {
+    Name: 'Git',
+    Badge: <DiGit
+      size="3rem"
+      style={
+        styles
+      }
+    />,
+  },
+  {
+    Name: 'VSCode',
+    Badge: <DiVisualstudio
+      size="3rem"
+      style={
+        styles
+      }
+    />,
+  },
+  {
+    Name: 'Swagger',
+    Badge: (<SiSwagger
+      size="3rem"
+      style={
+        styles
+      }
+    />),
   },
 ];
