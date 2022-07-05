7d11b68c9724e740998543de25adc53095834e9e
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-05 03:20:56 +0100

    Refactoring Technologies Section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,167 +1,35 @@
-import React from "react";
-import { List, ListItem, ListParagraph } from "./TechnologiesStyles";
-import {
-  DiFirebase,
-  DiReact,
-  DiZend,
-  DiNodejs,
-  DiMongodb,
-  DiAws,
-  DiLaravel,
-  DiJavascript,
-  DiHtml5,
-  DiCss3,
-  DiDocker,
-} from "react-icons/di";
+import React from 'react';
+
+import { List, ListItem, ListParagraph } from './TechnologiesStyles';
 import {
   Section,
   SectionTitle,
   SectionText,
-} from "../../styles/GlobalComponents/index";
-const Technologies = (props) => (
-  <div>
+} from '../../styles/GlobalComponents/index';
+import { TechnologiesAndTools } from '../../constants/constants';
+
+export default function Technologies() {
+  return (
     <Section id="tech">
-      <SectionTitle>Technologies</SectionTitle>
+      <SectionTitle>Technologies & Tools</SectionTitle>
       <SectionText
-        style={{ textAlign: "center", margin: "0 auto", color: "#3E0E12" }}
+        style={{ textAlign: 'left', margin: '0 auto', color: '#3E0E12' }}
       >
-        This section contains all the technologies and programming languages
+        This section contains all the technologies , programming languages and tools
         that I have learnt until now. I am constantly learning, therefore I may
         update this section more often.
       </SectionText>
       <List>
-        <ListItem>
-          <DiHtml5
-            size="3rem"
-            style={{
-              background: "#fff",
-              color: "#020061",
-              display: "flex",
-              alignItems: "center",
-              height: "100%",
-            }}
-          />
-          <ListParagraph>HTML5</ListParagraph>
-        </ListItem>
-        <ListItem>
-          <DiCss3
-            size="3rem"
-            style={{
-              background: "#fff",
-              color: "#020061",
-              display: "flex",
-              alignItems: "center",
-              height: "100%",
-            }}
-          />
-          <ListParagraph>CSS3</ListParagraph>
-        </ListItem>
-        <ListItem>
-          <DiJavascript
-            size="3rem"
-            style={{
-              background: "#fff",
-              color: "#020061",
-              display: "flex",
-              alignItems: "center",
-              height: "100%",
-            }}
-          />
-          <ListParagraph>JavaScript</ListParagraph>
-        </ListItem>
-        <ListItem>
-          <DiReact
-            size="3rem"
-            style={{
-              background: "#fff",
-              color: "#020061",
-              display: "flex",
-              alignItems: "center",
-              height: "100%",
-            }}
-          />
-          <ListParagraph>ReactJS</ListParagraph>
-        </ListItem>
-        <ListItem>
-          <DiFirebase
-            size="3rem"
-            style={{
-              background: "#fff",
-              color: "#020061",
-              display: "flex",
-              alignItems: "center",
-              height: "100%",
-            }}
-          />
-          <ListParagraph>Firebase</ListParagraph>
-        </ListItem>
-        <ListItem>
-          <DiAws
-            size="3rem"
-            style={{
-              background: "#fff",
-              color: "#020061",
-              display: "flex",
-              alignItems: "center",
-              height: "100%",
-            }}
-          />
-          <ListParagraph>AWS</ListParagraph>
-        </ListItem>
-        <ListItem>
-          <DiNodejs
-            size="3rem"
-            style={{
-              background: "#fff",
-              color: "#020061",
-              display: "flex",
-              alignItems: "center",
-              height: "100%",
-            }}
-          />
-          <ListParagraph>NodeJS</ListParagraph>
-        </ListItem>
-        <ListItem>
-          <DiDocker
-            size="3rem"
-            style={{
-              background: "#fff",
-              color: "#020061",
-              display: "flex",
-              alignItems: "center",
-              height: "100%",
-            }}
-          />
-          <ListParagraph>Docker</ListParagraph>
-        </ListItem>
-        <ListItem>
-          <DiZend
-            size="3rem"
-            style={{
-              background: "#fff",
-              color: "#020061",
-              display: "flex",
-              alignItems: "center",
-              height: "100%",
-            }}
-          />
-          <ListParagraph>Zend</ListParagraph>
-        </ListItem>
-        <ListItem>
-          <DiLaravel
-            size="3rem"
-            style={{
-              background: "#fff",
-              color: "#020061",
-              display: "flex",
-              alignItems: "center",
-              height: "100%",
-            }}
-          />
-          <ListParagraph>Laravel</ListParagraph>
-        </ListItem>
+        {
+          TechnologiesAndTools.map((technologie) => (
+            <ListItem key={technologie.Name}>
+              {technologie.Badge}
+              <ListParagraph>{technologie.Name}</ListParagraph>
+            </ListItem>
+          ))
+        }
+
       </List>
     </Section>
-  </div>
-);
-export default Technologies;
+  );
+}
